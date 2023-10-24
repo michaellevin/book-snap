cda20afc066ef5f3a773f0774844ed06c0078472
Michael Levin <michael.levi.fx@gmail.com>
2023-10-25 02:29:48 +0300

    print book

--- run.py
+++ run.py
@@ -7,4 +7,5 @@ library_ministry = booksnap.LibraryMinistry()
 library = library_ministry.build_library(".LIBRARY")
 url_book_6pages = "https://www.prlib.ru/item/331483"
 # library.download_book(url_book_6pages, resume=False)
-library.get_book(url_book_6pages)  # .pprint()
+book = library.get_book(url_book_6pages)  # .pprint()
+print(book)
