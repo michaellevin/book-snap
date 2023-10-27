6e5065710b3236888f3a4d58a8787c28547e84d4
Michael Levin <michael.levi.fx@gmail.com>
2023-10-28 02:51:52 +0300

    feat: UI added (PySide6)

---
+++ booksnap/ui/simple_viewer.py
@@ -0,0 +1,177 @@
+import sys
+import os
+import pprint
+
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+    QTextEdit,
+    QProgressBar,
+)
+from PySide6.QtCore import Qt, Slot, QMetaObject, Q_ARG
+
+from ..core import LibraryMinistry, BookState, IBook
+
+
+class SimpleApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.urls = [
+            "https://www.prlib.ru/item/331483",
+        ]
+        self.book = None
+        self.library_ministry = LibraryMinistry()
+        self.library = self.library_ministry.build_library(".LIBRARY")
+        self.library._clear_metadata()  # temporary
+        self.library.event_dispatcher.register_listener(
+            "register_book", self.register_book_event
+        )
+        self.library.event_dispatcher.register_listener(
+            "update_book_progress", self.update_book_progress_event
+        )
+        self.library.event_dispatcher.register_listener(
+            "images_downloaded", self.images_downloaded_event
+        )
+        self.library.event_dispatcher.register_listener(
+            "book_is_ready", self.book_is_ready_event
+        )
+
+        # Set main window properties
+        self.setWindowTitle("Simple Downloader")
+        self.setGeometry(300, 300, 600, 400)  # Set the position and size of the window
+
+        # Create a layout
+        layout = QVBoxLayout()
+
+        # Create a "Query " button
+        self.query_btn = QPushButton("Query")
+        self.query_btn.clicked.connect(self.query)
+        layout.addWidget(self.query_btn)
+
+        # Create a "Start Download" button
+        self.download_btn = QPushButton("Start Download")
+        self.download_btn.clicked.connect(self.start_download)
+        layout.addWidget(self.download_btn)
+
+        # Create a "Shutdown" button
+        self.shutdown_btn = QPushButton("Shutdown")
+        self.shutdown_btn.clicked.connect(self.shutdown)
+        layout.addWidget(self.shutdown_btn)
+
+        self.progress_bar = QProgressBar()
+        layout.addWidget(self.progress_bar)
+        self.progress_bar.setRange(0, 100)
+        stylesheet = """
+            QProgressBar {
+                border: 2px solid grey;
+                border-radius: 5px;
+                text-align: center; /* align the text in the center */
+                height: 6px; /* same as the chunk widths */
+                background-color: #4CAF50;  /* A nice shade of green */
+            
+                /* Text styling */
+                color: #FFFFFF;  /* White text for good readability */
+                font: bold 11px;  /* Bold, 11-point font (or whichever size suits your UI) */
+                text-align: center;  /* Center-aligned text */
+            }
+
+            QProgressBar::chunk {
+                background-color: #8BC34A; 
+                width: 10px; /* make "chunk" rectangles 10px wide */
+                margin: 1px; /* space between the chunks */
+            }
+        """
+        self.progress_bar.setStyleSheet(stylesheet)
+
+        self.text_edit = QTextEdit()
+        self.text_edit.setReadOnly(True)
+        layout.addWidget(self.text_edit)
+
+        self.open_btn = QPushButton("---")
+        layout.addWidget(self.open_btn)
+        self.open_btn.clicked.connect(self.open_book)
+
+        # Set the layout
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+    def query(self):
+        # Logic to query the library
+        print(f"Querying a book: {self.urls[0]}")
+        self.book = self.library.query_book(book_url=self.urls[0])
+        if self.book:
+            self.book.pprint()
+        else:
+            print("Book not found!")
+
+    @Slot()
+    def start_download(self):
+        # Logic to start the download
+        print("Download started!")  # Console log for demonstration
+
+        book = self.library.get_book(self.urls[0])
+        return book
+
+    @Slot()
+    def shutdown(self):
+        # Logic to handle shutdown
+        print("Shutting down!")  # Console log for demonstration
+        self.library._download_manager.abort(self.urls[0])
+        self.library._download_manager.shutdown(wait=False)
+
+    def update_textbox(self, book: IBook):
+        f_book_data = pprint.pformat(book.to_dict())
+        # Ensure that UI updates happen in the Qt main thread
+        QMetaObject.invokeMethod(
+            self.text_edit,
+            "setText",
+            Qt.QueuedConnection,
+            Q_ARG(str, f_book_data),
+        )
+
+    def register_book_event(self, book: IBook):
+        # Ensure that UI updates happen in the Qt main thread
+        self.open_btn.setText(f"Book {book.title} is fetched")
+        self.update_textbox(book)
+
+    def update_book_progress_event(self, book: IBook):
+        self.progress_bar.setValue(100 * book.progress_page / book.num_pages)
+
+    def images_downloaded_event(self, book: IBook):
+        self.progress_bar.setValue(100)
+        self.progress_bar.setFormat("PDF is ready")
+        self.update_textbox(book)
+
+    def book_is_ready_event(self, book: IBook):
+        # Ensure that UI updates happen in the Qt main thread
+        self.open_btn.setText(f"Open {book.title}")
+        self.book = book
+        self.update_textbox(book)
+
+    def open_book(self):
+        # Logic to open the book
+        if self.book is None:
+            print("Book not found!")
+        else:
+            if self.book.state == BookState.PDF_READY.value:
+                print(self.library.get_book_path(self.book))
+            else:
+                print("not ready")
+
+
+def main():
+    app = QApplication([])
+
+    window = SimpleApp()
+    window.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
--- run.py
+++ run.py
@@ -1,11 +1,17 @@
-import os
 import sys
 from pprint import pprint
 import booksnap
 
-library_ministry = booksnap.LibraryMinistry()
-library = library_ministry.build_library(".LIBRARY")
-url_book_6pages = "https://www.prlib.ru/item/331483"
-# library.download_book(url_book_6pages, resume=False)
-book = library.get_book(url_book_6pages)  # .pprint()
-print(book)
+# library_ministry = booksnap.LibraryMinistry()
+# library = library_ministry.build_library(".LIBRARY")
+# url_book_6pages = "https://www.prlib.ru/item/331483"
+# # library.download_book(url_book_6pages, resume=False)
+# book = library.get_book(url_book_6pages)  # .pprint()
+# print(book)
+
+from PySide6.QtWidgets import QApplication
+
+app = QApplication([])
+window = booksnap.SimpleApp()
+window.show()
+sys.exit(app.exec())
