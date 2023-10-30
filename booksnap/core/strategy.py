737bf014f5ef382a3c2e9cb04a27ca5ed81cf83b
Michael Levin <michael.levi.fx@gmail.com>
2023-10-30 03:02:16 +0200

    feat: add logic of SPHL library

--- booksnap/core/strategy.py
+++ booksnap/core/strategy.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from threading import Event
 from typing import Optional
 import requests
+from bs4 import BeautifulSoup
 import json
 from time import sleep
 import platform
@@ -203,19 +204,134 @@ class SPHLDownloadStrategy(DownloadStrategy):
     This class implements the DownloadStrategy interface for SPHL.
     """
 
-    def fetch_book_data(self, book_url: str, event_dispatcher: EventSystem) -> dict:
-        ...
+    SCAN_URL = "http://elib.shpl.ru/pages/{}/zooms/6"
+
+    def parse_html_for_key(
+        html_doc: str, key: str, is_title: bool = False
+    ) -> str | None:
+        soup = BeautifulSoup(html_doc, "html.parser")
+        tag_with_key = soup.find("td", text=key)
+        clean_value = None
+        if tag_with_key:
+            value_for_key = tag_with_key.find_next_sibling("td").get_text(strip=True)
+            clean_value = value_for_key.split("(")[0].strip()
+        elif is_title:
+            clean_value = soup.title.string if soup.title else None
+        return clean_value
+
+    def fetch_book_data(book_url: str, event_dispatcher: EventSystem) -> dict:
+        library_id = OnlineLibrary.PRLIB.value
+        library_book_id = book_url.split("/")[-1]
+
+        # Read html doc
+        # html_doc = system_call(f"curl {book_url}")
+        response = requests.get(book_url)
+        response.raise_for_status()
+        html_doc = response.text
+
+        # Parse it
+        title = SPHLDownloadStrategy.parse_html_for_key(html_doc, key="Заглавие")
+        if title is None:
+            title = library_book_id
+        author = SPHLDownloadStrategy.parse_html_for_key(html_doc, key="Автор")
+        year = SPHLDownloadStrategy.parse_html_for_key(html_doc, "Год издания")
+        # print(title, author, year)
+        ids = str(html_doc).split('links_z0":{')[1].split("}")[0]
+        ids = eval("{" + ids + "}")
+        list_of_ids = list(ids.keys())
+
+        # Emit signal
+        event_dispatcher.emit(
+            EventType.REGISTER_BOOK,  # "register_book",
+            book := IBook.create_instance(
+                {
+                    "url": book_url,
+                    "library_book_id": library_book_id,
+                    "library": library_id,
+                    "title": title,
+                    "author": author,
+                    "num_pages": len(list_of_ids),
+                    "year": year,
+                    "state": BookState.REGISTERED.value,
+                    "_tech_dict": {"image_ids": list_of_ids},
+                }
+            ),
+        )
+        return book
 
     def download_images(
-        self,
         book: IBook,
-        dest_folder: str,
+        root_folder: str,
         event_dispatcher: EventSystem,
         start_page: int = 0,
         timeout: int = 90,
+        main_event: Optional[Event] = None,
     ) -> IBook:
         # Logic for downloading a book from ELib
-        return 0
+        book_dest_folder = root_folder / book.title
+        book_dest_folder.mkdir(parents=True, exist_ok=True)
+
+        if start_page is not None:
+            image_ids = book.get_tech().image_ids
+            len_ids = len(image_ids)
+            for i in range(start_page, len_ids):
+                # Abort if main_event is set
+                if main_event is not None and main_event.is_set():
+                    logger.info("Download aborted")
+                    event_dispatcher.emit(
+                        EventType.UPDATE_BOOK_PROGRESS, book, state=BookState.TERMINATED
+                    )
+                    return book
+
+                # Pause
+                sleep(timeout)
+
+                # Construct the image path
+                image_path = book_dest_folder / f"{str(i).zfill(4)}.jpeg"
+                if image_path.exists():
+                    image_path.unlink()  # Remove if exists
+
+                # Download the image
+                im = image_ids[i]
+                im_address = SPHLDownloadStrategy.SCAN_URL.format(im)
+                try:
+                    im_cmd = f'curl {im_address} -o "{str(image_path)}"'
+                    logging.info(im_cmd)
+                    system_call(im_cmd)
+                    event_dispatcher.emit(
+                        EventType.UPDATE_BOOK_PROGRESS,
+                        book,
+                        state=BookState.DOWNLOADING,
+                        progress_page=i + 1,
+                    )
+                    logger.info(
+                        f"page:{i+1}/{book.num_pages}  |  url: {im_address} downloaded succesfully"
+                    )
+
+                except RuntimeError as err:
+                    logger.critical(err)
+                    event_dispatcher.emit(
+                        EventType.UPDATE_BOOK_PROGRESS, book, state=BookState.TERMINATED
+                    )
+
+            event_dispatcher.emit(
+                EventType.IMAGES_DOWNLOADED, book, state=BookState.DOWNLOAD_FINISHED
+            )
+
+        # * Convert images to PDF
+        # sleep(5)
+        try:
+            create_pdf(book_dest_folder, book.title)
+            event_dispatcher.emit(
+                EventType.BOOK_IS_READY, book, state=BookState.PDF_READY
+            )
+        except RuntimeError as err:
+            logger.critical(err)
+            event_dispatcher.emit(
+                EventType.IMAGES_DOWNLOADED, book, state=BookState.TERMINATED
+            )
+
+        return book
 
     @staticmethod
     def can_handle_url(book_url: str) -> bool:
