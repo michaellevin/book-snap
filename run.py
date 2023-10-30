e2ea9cbc441da3f729ec343d2647aeb958ac4662
Michael Levin <michael.levi.fx@gmail.com>
2023-10-30 14:13:53 +0200

    add just run code

--- run.py
+++ run.py
@@ -1,18 +1,16 @@
-import sys
 from pprint import pprint
 import booksnap
 
 library_ministry = booksnap.LibraryMinistry()
 library = library_ministry.build_library(".LIBRARY")
 url_book_6pages = "https://www.prlib.ru/item/331483"
-url_book_12pages = "http://elib.shpl.ru/ru/nodes/13552"
+url_book_24pages = "http://elib.shpl.ru/ru/nodes/13552"
 url_book_1page = "http://elib.shpl.ru/ru/nodes/12430"
-# library.download_book(url_book_6pages, resume=False)
-future_book = library.get_book(url_book_12pages)  # .pprint()
-future_book.result().pprint()
+future_book = library.get_book(url_book_24pages)
+future_book.result()
 
+# import sys
 # from PySide6.QtWidgets import QApplication
-
 # app = QApplication([])
 # window = booksnap.SimpleApp()
 # window.show()
