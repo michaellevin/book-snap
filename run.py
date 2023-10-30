2f4f78fdbab19a9063285645d9805025a2ddbf01
Michael Levin <michael.levi.fx@gmail.com>
2023-10-30 03:01:41 +0200

    testing

--- run.py
+++ run.py
@@ -2,16 +2,18 @@ import sys
 from pprint import pprint
 import booksnap
 
-# library_ministry = booksnap.LibraryMinistry()
-# library = library_ministry.build_library(".LIBRARY")
-# url_book_6pages = "https://www.prlib.ru/item/331483"
-# # library.download_book(url_book_6pages, resume=False)
-# book = library.get_book(url_book_6pages)  # .pprint()
-# print(book)
+library_ministry = booksnap.LibraryMinistry()
+library = library_ministry.build_library(".LIBRARY")
+url_book_6pages = "https://www.prlib.ru/item/331483"
+url_book_12pages = "http://elib.shpl.ru/ru/nodes/13552"
+url_book_1page = "http://elib.shpl.ru/ru/nodes/12430"
+# library.download_book(url_book_6pages, resume=False)
+future_book = library.get_book(url_book_12pages)  # .pprint()
+future_book.result().pprint()
 
-from PySide6.QtWidgets import QApplication
+# from PySide6.QtWidgets import QApplication
 
-app = QApplication([])
-window = booksnap.SimpleApp()
-window.show()
-sys.exit(app.exec())
+# app = QApplication([])
+# window = booksnap.SimpleApp()
+# window.show()
+# sys.exit(app.exec())
